9c2187e87e0f0bc93d794032621b9438e8d755a2
Ionică Bizău <bizauionica@yahoo.com>
2014-03-27 22:23:29 +0200

    Get fiddle and save the files

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var JSFiddleApi = require ("jsfiddle-api");
+// dependencies
+var JSFiddleApi = require ("jsfiddle-api")
+  , config      = require ("./config")
+  , fs          = require ("fs")
+  ;
 
+// get this fiddle
+JSFiddleApi.getFiddle (process.argv[2] + "/" + (config.fiddleVersion || ""), function (err, fiddleObj) {
 
+    // handle error
+    if (err) { return console.log (err); }
+
+    ++config.fiddleVersion;
+    fs.writeFileSync ("./config.json", JSON.stringify (config, null, 4));
+
+    fs.writeFileSync (process.argv[3] + "/index.html", JSON.stringify (config, null, 4));
+    fs.writeFileSync (process.argv[3] + "/functions.js", JSON.stringify (config, null, 4));
+    fs.writeFileSync (process.argv[3] + "/style.css", JSON.stringify (config, null, 4));
+});
